a84957507dfa1f0e607e83458fcade4f85ad0762
DominikSturhan <42314337+DominikSturhan@users.noreply.github.com>
2018-10-23 17:36:27 +0200

    Update ProposalStorage.sol

--- ProposalStorage.sol
+++ ProposalStorage.sol
@@ -1,13 +1,19 @@
 pragma solidity ^0.4.25;
 
+/**
+ * @title Proposal Storage
+ * @author Dominik Sturhan
+ * 
+ * @notice This contract stores all proposals.
+ */
 contract ProposalStorage {
     
-    struct Result {
-        // Counters
-        uint yea;
-        uint nay;
-    }
-    
+    /**
+     * @notice This structure defines what a proposal is. 
+     * 
+     * @dev The person creating the new proposal can only add a description. 
+     *  The other variables are set by the voting mechanisms.
+     */
     struct Proposal {
         // Information about the purpose of the proposal
         string description;
@@ -23,19 +29,67 @@ contract ProposalStorage {
         uint numberOfVotes;
         bool executed;
         bool proposalPassed;
-        
-        // Storages
         mapping (address => bool) voted;
-        Result result;
+        uint yea;
+        uint nay;
     }
     
-    Proposal[] public proposals;
+    // Internal storage for proposals
+    mapping (uint => Proposal) internal proposals;
+    // Counter for the ID
+    uint internal currentID;
     
+    /// External functions ///
+    
+    /**
+     * getProposal function
+     * 
+     * @notice Query the proposal belonging to the entered ID
+     * 
+     * @param _proposalID ID of the proposal
+     * @return All the import information about the proposal
+     */
+    function getProposal(
+        uint _proposalID
+    ) external view returns (string Description, string Status, 
+        uint WeightingDate, bool Passed, uint YEA, uint NAY){
+            
+        Proposal memory proposal = proposals[_proposalID];
+        string memory status;
+        
+        if(now >= proposal.startVotingPhase 
+            && now < proposal.endVotingPhase){
+             
+            status = "in voting phase";
+        } else if (now >= proposal.startRevealingPhase 
+            && now < proposal.endRevealingPhase){
+                
+            status = "in revealing phase";
+        } else if (now >= proposal.endRevealingPhase && !proposal.executed){
+            status = "wating for execution";
+        } else {
+            status = "closed";
+        }
+        
+        return (proposal.description, status, proposal.weightingDate, 
+            proposal.proposalPassed, proposal.yea, proposal.nay);
+    }
+    
+    /// Internal functions ///
+    
+    /**
+     * addProposal function
+     * 
+     * @notice Internal function to add a proposal to the storage
+     * @param _proposal The proposal to be added
+     * @return The ID of the added proposal
+     */
     function addProposal(
         Proposal _proposal
     ) internal returns (uint proposalID) {
-        proposalID = proposals.length++;
+        proposalID = currentID;
         proposals[proposalID] = _proposal;
+        currentID++;
         
         return proposalID;
     }
