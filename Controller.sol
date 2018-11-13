8a8efa004b0e9e274de34554a21998a8f5170c1c
DominikSturhan <42314337+DominikSturhan@users.noreply.github.com>
2018-11-13 15:05:46 +0100

    Update Controller.sol

--- Controller.sol
+++ Controller.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.4.25;
  * @title Controller
  * @author Dominik Sturhan
  * 
- * @notice This contract allows the owner of the token and the voting contract
- *  to controll the access
+ * @notice Certain functions should only be executed by a selected person. 
+ *  The controller contract inherits this function and defines who the owner is.
  */
 contract Controller {
     
@@ -14,10 +14,20 @@ contract Controller {
     /**
      * @notice Everyone is able to see the address of the 'owner'
      * 
-     * @dev The 'owner' could be a person who carries out orders on 
-     *  behalf of the shareholders
+     * @dev The 'owner' could be a person who carries out orders 
+     *  on behalf of the shareholders
      */
     address public owner;
+    
+    /// Modifiers ///
+    
+    /**
+     * @notice Modifier is needed to control access
+     */
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
     
     /// Functions ///
     
@@ -31,24 +41,14 @@ contract Controller {
     }
     
     /**
-     * transferOwnership function
+     * changeOwner function
      * 
      * @notice Transfers ownership of the contract
      * @param  _newOwner Address of the new owner
      */
-    function transferOwnership(
+    function changeOwner(
         address _newOwner
     ) public onlyOwner {
         owner = _newOwner;
-    }
-    
-    /// Modifier ///
-    
-    /**
-     * @notice Modifier is needed to control access
-     */
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
     }
 }
